67595406fbcaedbab3d2d0e6fbd00a39ae77d61e
hbseog <lllhades@naver.com>
2022-09-28 10:40:49 +0900

    age

--- c/hello.c
+++ c/hello.c
@@ -3,5 +3,4 @@
 int main() {
   printf("Hello Linux~!!\n");
   return 0;
-  
 }
--- hello.c
+++ hello.c
@@ -1,24 +1,51 @@
+#include <stdio.h>
+
+int main() {
+  printf("Hello Linux~!!\n");
+  return 0;
+}
 {
+
   "protocol":"sftp",
-  "host":"192.168.24.81",
+
+  "host":"192.168.24.35",
+
   "port":22,
+
   "user":"root",
+
   "pass":"1234",
+
   "promptForPass":"false",
+
   "remote":"/work",
+
   "local":"",
+
   "agent":"",
+
   "privatekey":"",
+
   "passphrase":"",
+
   "hosthash":"",
+
   "ignorehost":"true",
+
   "connTimeout":10000,
+
   "keepalive":100000,
+
   "keyboardInteractive":"false",
+
   "keyboardInteractiveForPass":"false",
+
   "remoteCommand":"",
+
   "remoteShell":"",
+
   "watch":[],
+
   "watchTimeout":500
 
 }
